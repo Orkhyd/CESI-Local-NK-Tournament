7a734bc3d60d24edd32ba6390da02be4ea95b79c
Marius Roelly <m.roelly@opticod.fr>
2025-03-26 14:01:15 +0100

    Refacto replicache plugin use in main

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from "./router";
 import { createVuestic } from "vuestic-ui";
 import "vuestic-ui/css";
 import "material-design-icons-iconfont/dist/material-design-icons.css";
+import replicache from "./replicache/replicache";
 
 const app = createApp(App);
 
@@ -18,6 +19,6 @@ app.use(
 if (window && window.electron) {
   app.config.globalProperties.$electron = window.electron;
 }
-
+app.use(replicache);
 
 app.mount("#app");
