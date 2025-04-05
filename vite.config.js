aa8ae820b184c1cbbed49493d0e4b88e6a7cce64
Marius Roelly <m.roelly@opticod.fr>
2025-04-05 21:52:04 +0200

    Add workbox config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,50 @@
 import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
   server: { allowedHosts: true },
   plugins: [
     vue(),
     vueDevTools(),
     VitePWA({
-      registerType: "autoUpdate", devOptions: { enabled: true }, workbox: { maximumFileSizeToCacheInBytes: 4097152 },
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+        suppressWarnings: true,
+        type: "module"
+      },
+      workbox: {
+        disableDevLogs: true,
+        maximumFileSizeToCacheInBytes: 4097152,
+        // Remove individual files from includeAssets and use globPatterns instead
+        globPatterns: [
+          '**/*.{js,css,html,png,jpg,jpeg,svg,ico}',
+          // Don't specify individual files here
+        ],
+        navigateFallback: "/",
+        navigateFallbackDenylist: [
+          /^\/src\/components\/.*\.vue/,
+          /^\/src\/replicache\/services\/Pool\/.*\.js/,
+          /^\/src\/functions\/.*\.js/
+        ],
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => url.pathname.startsWith('/src/'),
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'dynamic-components',
+              expiration: {
+                maxEntries: 60,
+                maxAgeSeconds: 60 * 60 * 24
+              }
+            }
+          }
+        ]
+      },
+      // Remove includeAssets completely
       manifest: {
         name: 'My Awesome App',
         short_name: 'MyApp',
@@ -22,19 +53,18 @@ export default defineConfig({
         display: "fullscreen",
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: '/pwa-192x192.png', // Make sure paths are correct
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: '/pwa-512x512.png', // Make sure paths are correct
             sizes: '512x512',
             type: 'image/png'
           }
         ]
       }
     }),
-
   ],
   resolve: {
     alias: {
