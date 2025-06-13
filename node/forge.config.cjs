f696b351494e48fba831aee00242dd28d140ca6c
Marius Roelly <m.roelly@opticod.fr>
2025-06-13 14:47:58 +0200

    remove fuzes

--- node/forge.config.cjs
+++ node/forge.config.cjs
@@ -1,9 +1,6 @@
-const { FusesPlugin } = require('@electron-forge/plugin-fuses');
-
 const config = {
   packagerConfig: {
     asar: true,
-    // Combine both resources into a single extraResource array
     extraResource: [
       './dist',
       {
@@ -35,18 +32,7 @@ const config = {
       name: '@electron-forge/plugin-auto-unpack-natives',
       config: {},
     },
-    {
-      name: '@electron-forge/plugin-fuses',
-      config: {
-        version: FusesPlugin.FUSES_VERSION,
-        [FusesPlugin.FUSES.runAsNode]: false,
-        [FusesPlugin.FUSES.enableCookieEncryption]: true,
-        [FusesPlugin.FUSES.enableNodeOptionsEnvironmentVariable]: false,
-        [FusesPlugin.FUSES.enableNodeCliInspectArguments]: false,
-        [FusesPlugin.FUSES.enableEmbeddedAsarIntegrityValidation]: true,
-        [FusesPlugin.FUSES.onlyLoadAppFromAsar]: true,
-      },
-    },
+    // Remove the fuses plugin for now to get it working
   ],
 };
 
